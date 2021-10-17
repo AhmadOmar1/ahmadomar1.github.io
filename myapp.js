e187d8bea5edfed212a57c8ba3b32913dc2d640e
AhmadOmar1 <77245617+AhmadOmar1@users.noreply.github.com>
2021-10-17 20:21:30 +0300

    Add files via upload

---
+++ myapp.js
@@ -0,0 +1,22 @@
+let x = prompt("Enter Your Name")
+alert("Hello " + x + " Enjoy Your Area calculate :D")
+
+
+function calculateSquareArea() {
+    const len = document.getElementById("sqaureInput").value;
+    document.getElementById("squareResult").innerText = len * len
+}
+
+function calculateRectArea() {
+    const L = document.getElementById("rectLInput").value;
+    const W = document.getElementById("rectWInput").value;
+    const result = W * L;
+    document.getElementById("rectResult").innerText = result;
+}
+
+function calculateCircleArea() {
+    const r = document.getElementById("circleInput").value;
+    const rArea = (r * r) * 3.14;
+    document.getElementById("circleResult").innerText = rArea;
+
+}
